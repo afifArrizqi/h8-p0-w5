b50f77ecba2bec54b7236767bbf8b70e715022ce
pippop21 <afifar38@gmail.com>
2020-03-20 13:30:37 +0700

    perbaikan excercise 6-MeleeRanged

--- 6-excercise-MeleeAttack.js
+++ 6-excercise-MeleeAttack.js
@@ -1,44 +1,45 @@
 function meleeRangedGrouping (str) {
   //your code here
-  const split1 = spliting(str, ",");
+  var method = {
+    spliting(arr, by){
+      let str = '';
+      const hasil = [];
+      for(let i = 0; i <= arr.length; i++){
+        if(arr[i] === by || i === arr.length) {
+          hasil.push(str);
+          str = '';
+        }else if(arr[i] !== by) str += arr[i];
+      }
+      return hasil;
+    },
+    grouping(arr, indexOfGroup, indexToBeGrouped){
+      const obj = {
+        Ranged: [],
+        Melee: []
+      }
+      const arrHasil = [];
+      for(let i = 0; i < arr.length; i++){
+        if(!obj[arr[i][indexOfGroup]]){
+          obj[arr[i][indexOfGroup]] = [arr[i][indexToBeGrouped]];
+        }else obj[arr[i][indexOfGroup]].push(arr[i][indexToBeGrouped]);
+      }
+      for(let key in obj) {
+        // if(obj[key][0].length)
+          arrHasil.push(obj[key]);
+      }
+      if(obj["Melee"].length === 0 && obj["Ranged"].length === 0) return [];
+      return arrHasil;
+    }
+  }
+  const split1 = method.spliting(str, ",");
   const split2 = [];
   for(let i = 0; i < split1.length; i++){
-    split2.push(spliting(split1[i], "-"));
+    split2.push(method.spliting(split1[i], "-"));
   }
   // if(!str.length) return "''";
-  return grouping(split2, 1, 0);
+  return method.grouping(split2, 1, 0);
 }
 
-function spliting(arr, by){
-  let str = '';
-  const hasil = [];
-  for(let i = 0; i <= arr.length; i++){
-    if(arr[i] === by || i === arr.length) {
-      hasil.push(str);
-      str = '';
-    }else if(arr[i] !== by) str += arr[i];
-  }
-  return hasil;
-}
-
-function grouping(arr, indexOfGroup, indexToBeGrouped){
-  const obj = {
-    Ranged: [],
-    Melee: []
-  }
-  const arrHasil = [];
-  for(let i = 0; i < arr.length; i++){
-    if(!obj[arr[i][indexOfGroup]]){
-      obj[arr[i][indexOfGroup]] = [arr[i][indexToBeGrouped]];
-    }else obj[arr[i][indexOfGroup]].push(arr[i][indexToBeGrouped]);
-  }
-  for(let key in obj) {
-    // if(obj[key][0].length)
-      arrHasil.push(obj[key]);
-  }
-  if(obj["Melee"].length === 0 && obj["Ranged"].length === 0) return [];
-  return arrHasil;
-}
 
 // TEST CASE
 
